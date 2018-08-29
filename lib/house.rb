40d64ef3582bf5bf9131b14dea6d5f9c4e9b6c0b
normanrs <normanrs@live.com>
2018-08-29 13:59:12 -0600

    iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -15,4 +15,20 @@ class House
       @rooms << room
     end
 
+    def rooms_from_category(type)
+      rooms_of_type = []
+      @rooms.each do |room|
+        rooms_of_type << room if room.category == type
+        end
+      rooms_of_type
+    end
+
+    def area
+      total_area = 0
+      @rooms.each do |room|
+        total_area += room.area
+      end
+      total_area
+    end
+
 end
--- test/test_house.rb
+++ test/test_house.rb
@@ -46,4 +46,49 @@ class HouseTest < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_it_can_return_bedrooms
+  house = House.new("$400000", "123 sugar lane")
+  room_1 = Room.new(:bedroom, 10, 13)
+  room_2 = Room.new(:bedroom, 11, 15)
+  room_3 = Room.new(:living_room, 25, 15)
+  room_4 = Room.new(:basement, 30, 41)
+  house.add_room(room_1)
+  house.add_room(room_2)
+  house.add_room(room_3)
+  house.add_room(room_4)
+  actual = house.rooms_from_category(:bedroom).count
+  expected = 2
+  assert_equal expected, actual
+  end
+
+  def test_it_can_return_basement
+  house = House.new("$400000", "123 sugar lane")
+  room_1 = Room.new(:bedroom, 10, 13)
+  room_2 = Room.new(:bedroom, 11, 15)
+  room_3 = Room.new(:living_room, 25, 15)
+  room_4 = Room.new(:basement, 30, 41)
+  house.add_room(room_1)
+  house.add_room(room_2)
+  house.add_room(room_3)
+  house.add_room(room_4)
+  actual = house.rooms_from_category(:basement).count
+  expected = 1
+  assert_equal expected, actual
+  end
+
+  def test_it_can_calculate_area
+  house = House.new("$400000", "123 sugar lane")
+  room_1 = Room.new(:bedroom, 10, 13)
+  room_2 = Room.new(:bedroom, 11, 15)
+  room_3 = Room.new(:living_room, 25, 15)
+  room_4 = Room.new(:basement, 30, 41)
+  house.add_room(room_1)
+  house.add_room(room_2)
+  house.add_room(room_3)
+  house.add_room(room_4)
+  actual = house.area
+  expected = 1900
+  assert_equal expected, actual
+  end
+
 end
