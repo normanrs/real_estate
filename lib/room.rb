eff10d90183c242a7d0c5fb3b8d19c0c4d8e9890
normanrs <normanrs@live.com>
2018-08-29 13:14:15 -0600

    basic working class

--- lib/room.rb
+++ lib/room.rb
@@ -1,2 +1,13 @@
 class Room
+
+  	attr_reader :category,
+  							:width,
+                :depth
+
+  	def initialize(category, length, width)
+  		@category = category
+      @length = length
+      @width = width
+  	end
+
 end
--- test/test_room.rb
+++ test/test_room.rb
@@ -4,8 +4,12 @@ require './lib/room.rb'
 
 class RoomTest < Minitest::Test
   def test_it_exists
-
+    room = Room.new(1, 2, 3)
     assert_instance_of Room, room
   end
 
+  def test_it_takes_arguments
+    room = Room.new(:bedroom, 10, 13)
+    assert_instance_of Room, room
+  end
 end
